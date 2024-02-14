b7af3763d6c220ed94603e7ec1303acbbdf6641a
RatchanonNetsuwan <s6406021611034@email.kmutnb.ac.th>
2024-02-14 10:27:39 +0700

    up

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const express = require('express');
 const axios  = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require("path")
 const e = require('express');
 
 // Base URL for the API
@@ -17,7 +18,7 @@ const e = require('express');
 const base_url = "http://node57032-ratchanon-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
-app.set("views" , path.join(__dirname , "/public/views"))
+app.set("views" ,path.join(__dirname , "/public/views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
