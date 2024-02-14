d2987017a20faf38bd3c5a0e81fd8d648daa98f7
RatchanonNetsuwan <s6406021611034@email.kmutnb.ac.th>
2024-02-14 10:22:32 +0700

    up view

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ const e = require('express');
 // Base URL for the API
 // const base_url = "https;//api.example.com"
 // const base_url = "http://localhost:3000";
-const base_url = "http://node57032-ratchanon-noderest.proen.app.ruk-com.cloud";
+const base_url = "http://node57135-thitirat-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set("views" , path.join(__dirname , "/public/views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
